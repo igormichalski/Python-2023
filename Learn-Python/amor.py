1ad42bf2b2e29ea6c588a4aad478fa7a558442d5
Igor Roberto <igorroberto2312@gmail.com>
2023-03-26 16:50:23 -0400

    Update amor.py

--- Learn-Python/amor.py
+++ Learn-Python/amor.py
@@ -1,4 +1,4 @@
 nome1 = input("Informe o primeiro nome: ")
 nome2 = input("Informe o segundo nome: ")
-porcetagem = input("Diga uma porcentagem ao amor: ")
+porcetagem = int(input("Diga uma porcentagem ao amor: "))
 print(f"{nome1} ama {nome2} {porcetagem}%")
