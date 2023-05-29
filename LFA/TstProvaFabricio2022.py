d73a54914f2ea31d0b5b802a2a72fd8494f1f887
Igor Roberto <igorroberto2312@gmail.com>
2023-05-29 18:26:10 -0400

    Create TstProvaFabricio2022.py

---
+++ LFA/TstProvaFabricio2022.py
@@ -0,0 +1,16 @@
+sequencia = input("Infome uma sequencia: ")
+tamanho_sequencia = len(sequencia)
+aceita = 0
+i=0
+
+while(i<tamanho_sequencia):
+    if sequencia[i] == "1":
+        aceita = 1
+    else:
+        aceita = 0
+    i += 1
+
+if aceita == 1:
+    print("Aceito")
+else:
+    print("Recusado")
