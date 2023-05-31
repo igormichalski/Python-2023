942adc43c36eb01e9a7fdad05648fbfc1479516f
Igor Roberto <igorroberto2312@gmail.com>
2023-05-31 16:13:25 -0400

    Update and rename Janela+API.py to cotacoes.py

--- cotacoes.py
+++ cotacoes.py
@@ -5,8 +5,6 @@ def atualizar():
     return requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").json()
 def valorDolar():
     cotacao = atualizar()
-    valor = entrada.get()
-    valor = int(valor)
     valor_Dolar = cotacao['USDBRL']['bid']
     textTotal["text"] = valor_Dolar
 
@@ -14,34 +12,27 @@ def valorDolar():
 def valorEuro():
     cotacao = atualizar()
     valor_Euro = cotacao["EURBRL"]["bid"]
-    textTotal["text"] = valor_Euro*entrada
+    textTotal["text"] = valor_Euro
 
 def valorBTC():
     cotacao = atualizar()
     valor_BTC = cotacao["BTCBRL"]["bid"]
-    textTotal["text"] = valor_BTC*entrada
+    textTotal["text"] = valor_BTC
 
 
 janela = Tk()
+janela.geometry("130x115")
 janela.title("Conversor de moeda")
-janela.geometry("400x300")
 
 botaoDolar = Button(janela, text="Dolar => Real", command=valorDolar)
 botaoEuro = Button(janela, text="Euro => Real", command=valorEuro)
 botaoBTC = Button(janela, text="Bitcoin => Real", command=valorBTC)
 
-botaoDolar.grid(column=1,row=2, padx=150, pady=2)
-botaoEuro.grid(column=1,row=3, padx=150, pady=2)
-botaoBTC.grid(column=1,row=4, padx=150, pady=2)
+botaoDolar.grid(column=1,row=2)
+botaoEuro.grid(column=1,row=3)
+botaoBTC.grid(column=1,row=4)
 
 textTotal = Label(janela, text="")
 textTotal.grid(column=1,row=5)
 
-entrada = Entry(janela)
-entrada.place(x=150, y=110,width=100, height=20)
-
-
 janela.mainloop()
-
-
-
